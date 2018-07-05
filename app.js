7faec70156de5b9d9a69b0ff7f55a29ccefa3d36
mhtsbt <matthias.hutsebaut@gmail.com>
2018-07-05 21:18:09 +0200

    some exp with vue

--- app.js
+++ app.js
@@ -4,20 +4,70 @@ var songs = [];
 
 var player = document.getElementById('player');
 
+var currentPlayingSong;
+
 var example1 = new Vue({
     el: '#example-1',
     data: {
-        items: songs
+        songs: songs
     },
     methods: {
-        playSong: function (song) {
-            console.log(song);
+
+    }
+});
+
+//
+
+var bus = new Vue();
+
+Vue.component('song-row', {
+    props: ['song'],
+    template: '<div style="padding:10px;" v-on:click="playSong" v-bind:class="{\'playing\':playing}">{{song.track}} {{song.title}} {{playing}}</div>',
+    data: function () {
+        return { playing: false };
+    },
+    created: function () {
+        console.log("hello!");
+
+        bus.$on("changedSong", w => { this.playing = false; console.log("song changed", this); });
+
+    },
+    methods: {
+
+        playSong: function () {
+
+            var song = this.song;
+
+            if (currentPlayingSong) {
+                currentPlayingSong.playing = false;
+            }
+
+            bus.$emit('changedSong', {});
+
+            song.playing = true;
+            this.playing = true;
+
+            currentPlayingSong = song;
+
 
             player.src = "http://localhost:9000/" + song.hash;
             player.play();
+
+
+
         }
     }
 });
+
+/*
+var example2 = new Vue.component({
+    el: '.example-2',
+    data: {
+        songs: songs
+    },
+   
+    }
+});*/
 
 
 /*
